e609a4cdd2425a47282df862df62abdfed816a16
fishery <i@ovo.so>
2020-11-22 15:48:21 -0600

    add boilerplate accessory

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,151 +1,113 @@
-import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
+import {
+	Service,
+	PlatformAccessory,
+	CharacteristicValue,
+	CharacteristicEventTypes,
+	CharacteristicSetCallback,
+	CharacteristicGetCallback,
+} from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { SwitchmateSwitchPlatform } from './platform';
 
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
-export class ExamplePlatformAccessory {
-  private service: Service;
+import { SwitchmateDevice } from './switchmateDevice';
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+export class SwitchAccessory {
+	private service: Service;
+	private batteryService: Service;
 
-  constructor(
-    private readonly platform: ExampleHomebridgePlatform,
-    private readonly accessory: PlatformAccessory,
-  ) {
+	// use false -> off and true -> on
+	private switchState = false;
 
-    // set accessory information
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+	private batteryState = {
+		level: 100,
+		charging: 2, // not chargable
+		low_battery: 0, // normal
+	};
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+	constructor(
+		private readonly platform: SwitchmateSwitchPlatform,
+		private readonly accessory: PlatformAccessory,
+		private readonly switchmateDevice: SwitchmateDevice,
+	) {
+		// setup switch service
+		this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
+		this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+		this.service.getCharacteristic(this.platform.api.hap.Characteristic.On).updateValue(this.switchState);
+		this.service
+			.getCharacteristic(this.platform.api.hap.Characteristic.On)
+			.on(CharacteristicEventTypes.GET, this.getState.bind(this))
+			// eslint-disable-next-line @typescript-eslint/no-misused-promises
+			.on(CharacteristicEventTypes.SET, this.setState.bind(this));
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+		// setup battery service
+		this.batteryService = this.accessory.getService(this.platform.Service.Battery) || this.accessory.addService(this.platform.Service.Battery);
+		this.batteryService.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name + ' Battery');
+		this.batteryService.getCharacteristic(this.platform.api.hap.Characteristic.BatteryLevel).updateValue(this.batteryState.level);
+		this.batteryService.getCharacteristic(this.platform.api.hap.Characteristic.ChargingState).updateValue(this.batteryState.charging);
+		this.batteryService.getCharacteristic(this.platform.api.hap.Characteristic.StatusLowBattery).updateValue(this.batteryService.low_battery);
+		this.batteryService
+			.getCharacteristic(this.platform.api.hap.Characteristic.BatteryLevel)
+			.on(CharacteristicEventTypes.GET, this.getState.bind(this));
+		this.batteryService
+			.getCharacteristic(this.platform.api.hap.Characteristic.ChargingState)
+			.on(CharacteristicEventTypes.GET, this.getState.bind(this));
+		this.batteryService
+			.getCharacteristic(this.platform.api.hap.Characteristic.StatusLowBattery)
+			.on(CharacteristicEventTypes.GET, this.getState.bind(this));
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+		// set accessory information
+		this.switchmateDevice.getInfomationCharacteristics().then((deviceInformation) => {
+			this.platform.log.debug('successfully get device information');
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
+			this.accessory.getService(this.platform.Service.AccessoryInformation)!
+				.setCharacteristic(this.platform.Characteristic.Manufacturer, deviceInformation.manufacturer)
+				.setCharacteristic(this.platform.Characteristic.Model, 'Smart Switch')
+				.setCharacteristic(this.platform.Characteristic.SerialNumber, deviceInformation.serial)
+				.setCharacteristic(this.platform.Characteristic.HardwareRevision, deviceInformation.hardwareRevision)
+				.setCharacteristic(this.platform.Characteristic.SoftwareRevision, deviceInformation.softwareRevision)
+				.setCharacteristic(this.platform.Characteristic.FirmwareRevision, deviceInformation.firmwareRevision);
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+			void this.poll();
+		}).catch((error) => this.platform.log.error('Failed to get device information: %s', error));
+	}
 
+	private getState(callback: CharacteristicGetCallback): void {
+		callback(undefined, this.switchState);
+	}
 
-    /**
-     * Creating multiple services of the same type.
-     * 
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     * 
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
+	private async setState(value: CharacteristicValue, callback: CharacteristicSetCallback): Promise<void> {
+		this.platform.log.debug('setting switch state to %s', value as boolean);
 
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
+		callback(null);
+	}
 
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+	private getBatteryLevel(callback: CharacteristicGetCallback): void {
+		callback(undefined, this.batteryState.level);
+	}
 
-    /**
-     * Updating characteristics values asynchronously.
-     * 
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     * 
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
+	private getChargingState(callback: CharacteristicGetCallback): void {
+		callback(undefined, this.batteryState.charging);
+	}
 
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+	private getLowBatteryState(callback: CharacteristicGetCallback): void {
+		callback(undefined, this.batteryState.low_battery);
+	}
 
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
-  }
+	private async poll() {
+		// Loop forever.
+		for (; ;) {
+			this.platform.log.debug('refreshing...');
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+			
 
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+			// Sleep until our next update.
+			await this.sleep(10);
+		}
+	}
 
-    this.platform.log.debug('Set Characteristic On ->', value);
-
-    // you must call the callback function
-    callback(null);
-  }
-
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   * 
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   * 
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  getOn(callback: CharacteristicGetCallback) {
-
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // you must call the callback function
-    // the first argument should be null if there were no errors
-    // the second argument should be the value to return
-    callback(null, isOn);
-  }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
-
-    // you must call the callback function
-    callback(null);
-  }
+	// Emulate a sleep function.
+	private sleep(s: number): Promise<NodeJS.Timeout> {
+		return new Promise(resolve => setTimeout(resolve, s * 1000));
+	}
 
 }
